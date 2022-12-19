4e287bbc59784cf84869031fe6ef92cebcae9e52
tjmac21 <tjmacu@gmail.com>
2022-12-18 21:42:04 -0500

    canvas drawing (once the boxes have been calculated) is now occuring in the service worker, still need to get the box calculation to occur off the main thread

--- src/background.js
+++ src/background.js
@@ -10,6 +10,14 @@ const sendPayload = (type, payload) => {
   });
 };
 
+function blobToBase64(blob) {
+  return new Promise((resolve, _) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.readAsDataURL(blob);
+  });
+}
+
 const CONTEXT_MENU_ID = 'facefaceface';
 chrome.contextMenus.create({
   title: 'BLOCK DAT ISH',
@@ -20,5 +28,26 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === CONTEXT_MENU_ID) {
     // Handle click event here
     sendPayload('BOOMERANG', { url: info.srcUrl });
+  }
+});
+
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+  if (request.message == 'convert_image_url_to_blob') {
+    var canvas = new OffscreenCanvas(request.width, request.height);
+    const iblob = await fetch(request.url).then((r) => r.blob());
+    const img = await createImageBitmap(iblob);
+    canvas.getContext('2d').drawImage(img, 0, 0);
+
+    request.boxes.forEach((box) =>
+      canvas.getContext('2d').fillRect(box.x, box.y, box.width, box.height)
+    );
+    if (canvas.convertToBlob !== undefined) {
+      const imgBlob = await canvas.convertToBlob();
+      console.log('wtf');
+      const base64Blob = await blobToBase64(imgBlob);
+      console.log(imgBlob);
+
+      sendPayload('REPLACE', { base64Blob });
+    }
   }
 });
--- src/contentScript.js
+++ src/contentScript.js
@@ -10,6 +10,10 @@ import * as faceapi from '../build/dist/face-api.esm';
   console.log(model);
   await faceapi.nets.ssdMobilenetv1.loadFromUri(model);
   console.log('YEEEEHAW');
+
+  // const img = await chrome.runtime.getURL('/images/faces.jpeg');
+  // await detectFaces(img);
+  // console.log(primes)
 })();
 
 function blobToBase64(blob) {
@@ -20,61 +24,39 @@ function blobToBase64(blob) {
   });
 }
 
-const detectFaces = async (canvas, ctx, img, cancelAnimation) => {
-  const start = Date.now();
+class Tester extends OffscreenCanvas {
+  constructor() {
+    super(1000, 1000);
+  }
+}
+
+const detectFaces = async (img) => {
   console.log('Detecting faces');
   try {
+    faceapi.env.monkeyPatch({
+      Canvas: Tester,
+    });
     const detections = await faceapi.detectAllFaces(img);
-
-    console.log('Detection done');
-    cancelAnimation();
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    canvas.width = img.naturalWidth;
-    canvas.height = img.naturalHeight;
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = 'black';
-    ctx.fillStyle = 'black';
-
-    // redraw image with black box
-    ctx.drawImage(img, 0, 0);
-    detections.forEach((det) => {
-      // const wRatio = canvas.width / det.imageWidth;
-      // const hRatio = canvas.height / det.imageHeight;
-      // ctx.strokeRect(
-      //   det.box.x * wRatio
-      //   det.box.y * hRatio
-      //   det.box.width * wRatio
-      //   det.box.height * hRatio
-      // );
-      ctx.fillRect(det.box.x, det.box.y, det.box.width, det.box.height);
-    });
-    console.log('Drawing done');
-
-    canvas[
-      canvas.convertToBlob
-        ? 'convertToBlob' // specs
-        : 'toBlob' // current Firefox
-    ](async (imgBlob) => {
-      const base64Blob = await blobToBase64(imgBlob);
-
-      img.parentNode.removeChild(canvas);
-      img.src = base64Blob;
-    });
+    return detections;
   } catch (err) {
     console.log(err);
   }
-  console.log(`Done in ${Date.now() - start}ms`);
+  return null;
 };
 
-const doTheThing = (url, img) => {
+const prepareCanvas = (img) => {
   // Get the canvas element and its 2D context
   const canvas = document.createElement('canvas');
+  canvas.id = 'BOOMSHAKALAKA';
   const ctx = canvas.getContext('2d');
 
   // Set the dimensions of the canvas to match the dimensions of the image
   canvas.width = img.width;
   canvas.height = img.height;
+
+  // Insert the canvas element into the DOM, right after the image
+  img.setAttribute('cross-origin', 'anonymous');
+  img.parentNode.insertBefore(canvas, img.nextSibling);
 
   // Set the line width and stroke style for the loading animation
   ctx.lineWidth = 5;
@@ -106,23 +88,131 @@ const doTheThing = (url, img) => {
     requestID = requestAnimationFrame(animate);
   }
 
-  // Insert the canvas element into the DOM, right after the image
-  img.crossOrigin = 'anonymous';
-  img.parentNode.insertBefore(canvas, img.nextSibling);
-
-  // Set the position and z-index of the canvas
-  // canvas.style.position = 'absolute';
-  // canvas.style.zIndex = '1';
-
   // Start the animation
   animate();
 
-  detectFaces(canvas, ctx, img, () => cancelAnimationFrame(requestID));
+  return {
+    canvas: canvas,
+    cancelAnimation: () => cancelAnimationFrame(requestID),
+  };
 };
+
+const drawOnCanvas = async (boxes, img, c) => {
+  const canvas = new OffscreenCanvas(img.naturalWidth, img.naturalHeight);
+  const ctx = canvas.getContext('2d');
+  // console.log(ctx);
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // canvas.width = img.naturalWidth;
+  // canvas.height = img.naturalHeight;
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = 'black';
+  ctx.fillStyle = 'black';
+
+  // redraw image with black box
+  ctx.drawImage(img, 0, 0);
+  boxes.forEach((det) => {
+    // const wRatio = canvas.width / det.imageWidth;
+    // const hRatio = canvas.height / det.imageHeight;
+    // ctx.strokeRect(
+    //   det.box.x * wRatio
+    //   det.box.y * hRatio
+    //   det.box.width * wRatio
+    //   det.box.height * hRatio
+    // );
+    ctx.fillRect(det.box.x, det.box.y, det.box.width, det.box.height);
+  });
+  console.log('Drawing done');
+
+  if (canvas.toBlob !== undefined) {
+    canvas.toBlob(async (imgBlob) => {
+      console.log('wtf');
+      const base64Blob = await blobToBase64(imgBlob);
+      console.log(imgBlob);
+      img.parentNode.removeChild(canvas);
+
+      img.src = base64Blob;
+    });
+  }
+  if (canvas.convertToBlob !== undefined) {
+    const imgBlob = await canvas.convertToBlob();
+    console.log('wtf');
+    const base64Blob = await blobToBase64(imgBlob);
+    console.log(imgBlob);
+
+    img.src = base64Blob;
+  }
+};
+
+const doHalfInClient = async (img) => {
+  const start = Date.now();
+  try {
+    const { canvas, cancelAnimation } = prepareCanvas(img);
+
+    // Insert the canvas element into the DOM, right after the image
+    img.setAttribute('cross-origin', 'anonymous');
+    img.parentNode.insertBefore(canvas, img.nextSibling);
+
+    const boxes = await detectFaces(img);
+
+    chrome.runtime.sendMessage({
+      action: 'getImg',
+      canvasId: 'BOOMSHAKALAKA',
+      boxes,
+      dimensions: {
+        height: img.naturalHeight,
+        width: img.naturalWidth,
+      },
+    });
+
+    cancelAnimation();
+
+    // drawOnCanvas(boxes, img, canvas);
+  } catch (err) {
+    console.log(err);
+  }
+  console.log(`Done in ${Date.now() - start}ms`);
+};
+
+const doAllInClient = async (img, url) => {
+  const start = Date.now();
+  try {
+    // const { canvas, cancelAnimation } = prepareCanvas(img);
+
+    const boxes = await detectFaces(img);
+
+    // cancelAnimation();
+
+    img.classList.add('EXTREMELY_FACE');
+    chrome.runtime.sendMessage({
+      message: 'convert_image_url_to_blob',
+      url: url,
+      height: img.naturalHeight,
+      width: img.naturalWidth,
+      boxes: boxes.map((det) => ({
+        x: det.box.x,
+        y: det.box.y,
+        width: det.box.width,
+        height: det.box.height,
+      })),
+    });
+    // drawOnCanvas(boxes, img, null);
+    // img.parentNode.removeChild(canvas);
+  } catch (err) {
+    console.log(err);
+  }
+  console.log(`Done in ${Date.now() - start}ms`);
+};
+
 // Listen for message
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.type === 'HMN_LOG') {
     console.log('Log: ', request.payload);
+  } else if (request.type === 'REPLACE') {
+    console.log('replace: ', request.payload);
+    const imgs = document.getElementsByClassName('EXTREMELY_FACE');
+    if (imgs.length === 1 && request.payload.base64Blob) {
+      imgs[0].src = request.payload.base64Blob;
+    }
   } else if (request.type === 'BOOMERANG') {
     console.log('BOOM: ', request.payload);
 
@@ -141,13 +231,15 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
         );
         if (im.currentSrc == request.payload.url) {
           console.log('WINNER!!!', im);
-          doTheThing(request.payload.url, im);
+          // doHalfInClient(im);
+          doAllInClient(im, request.payload.url);
         }
       });
     }
     // Check if the image was found
     if (img) {
-      doTheThing(request.payload.url, img);
+      // doHalfInClient(img);
+      doAllInClient(img, request.payload.url);
     }
   } else {
     console.log('yoooo', { request });
@@ -158,3 +250,9 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   sendResponse({});
   return true;
 });
+
+// plan is to spawn a service worker and do the drawing there via this: https://stackoverflow.com/a/19896047
+// this is great since it also unblocks the main thread
+// additionally I want to try using puire css loaders like this: https://cssloaders.github.io/
+// another issue to solve is the problem if its not an img tag
+// another issue to solve it the problem if the img tag cant be right-clicked because its behind another element
